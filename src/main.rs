dabf0fd9f3a17d0ec91234f3bb9071fa04434061
Basile du Plessis <basile.duplessis@gmail.com>
2021-04-14 22:02:22 +0200

    Test ndarray

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use ndarray::prelude::*;
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn predict(x: &Array2<u64>, y: &Array2<u64>) -> Array2<u64> {  
+    x.dot(y)
+}
+
+#[test]
+fn test_predict() {
+    let x = array![
+        [1, 2],
+        [3, 4]
+    ];
+    let y = array![
+        [1, 2],
+        [3, 4]
+    ];
+    let x_multiply_y = array![
+        [7, 10],
+        [15, 22]
+    ];
+
+    assert_eq!(predict(&x,&y), x_multiply_y);
 }
