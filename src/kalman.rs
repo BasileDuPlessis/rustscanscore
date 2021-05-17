7768424e39652f608247981113e271dd29d2f759
Basile du Plessis <basile.duplessis@gmail.com>
2021-05-18 00:56:16 +0200

    Kalman filter is setup

---
+++ src/kalman.rs
@@ -0,0 +1,266 @@
+use crate::M2x2;
+use crate::M2x1;
+
+
+fn dot_to_2x1(a: &M2x2, b: &M2x1) -> M2x1 {
+    (
+        (a.0.0 * b.0.0 + a.0.1 * b.1.0, ), 
+        (a.1.0 * b.0.0 + a.1.1 * b.1.0, )
+    )
+}
+
+fn dot_to_2x2(a: &M2x2, b: &M2x2) -> M2x2 {
+    (
+        (a.0.0 * b.0.0 + a.0.1 * b.1.0, a.0.0 * b.0.1 + a.0.1 * b.1.1), 
+        (a.1.0 * b.0.0 + a.1.1 * b.1.0, a.1.0 * b.0.1 + a.1.1 * b.1.1)
+    )
+}
+
+fn transpose(a: &M2x2) -> M2x2 {
+    (
+        (a.0.0, a.1.0),
+        (a.0.1, a.1.1)
+    )
+}
+
+fn add_2x2(a: &M2x2, b: &M2x2) -> M2x2 {
+    ((a.0.0 + b.0.0, a.0.1 + b.0.1), (a.1.0 + b.1.0, a.1.1 + b.1.1))
+}
+
+fn add_2x1(a: &M2x1, b: &M2x1) -> M2x1 {
+    ((a.0.0 + b.0.0, ), (a.1.0 + b.1.0, ))
+}
+
+fn sub_2x1(a: &M2x1, b: &M2x1) -> M2x1 {
+    ((a.0.0 - b.0.0, ), (a.1.0 - b.1.0, ))
+}
+
+fn sub_2x2(a: &M2x2, b: &M2x2) -> M2x2 {
+    ((a.0.0 - b.0.0, a.0.1 - b.0.1), (a.1.0 - b.1.0, a.1.1 - b.1.1))
+}
+
+fn inv_2x2(a: &M2x2) -> M2x2 {
+    let det = a.0.0 * a.1.1 - a.1.0 * a.0.1;
+    
+    if det == 0.0 {panic!("Could not inverse matrix with determinant equal to zero");}
+
+    (
+        (a.1.1 / det, -a.1.0 / det),
+        (-a.0.1 / det, a.0.0 / det)
+    )
+}
+
+/**
+x : The mean state estimate of the previous step (k −1)
+p : The state covariance of previous step (k −1).
+a : The transition n n × matrix.
+*/
+
+pub fn predict(x: &M2x1, p: &M2x2, a: &M2x2) -> (M2x1, M2x2) {    
+    let x = dot_to_2x1(&a, &x);
+    let p = dot_to_2x2(&a, &dot_to_2x2(&p, &transpose(&a)));
+    let p_diag = (
+        (p.0.0, 0.0),
+        (0.0, p.1.1)
+    );
+    (x, p_diag)
+}
+
+/**
+x : The mean state estimate of the previous step (k −1).
+p : The state covariance of previous step (k −1).
+y : The measurement matrix.
+h : The state matrix.
+r : The measurement noise covariance matrix.
+*/
+pub fn update(x: &M2x1, p: &M2x2, y: &M2x1, h: &M2x2, r: &M2x2) -> (M2x1, M2x2) {
+    let k_num = dot_to_2x2(&p, &transpose(&h));
+    let k_den =
+        &add_2x2(
+            &dot_to_2x2(
+                &dot_to_2x2(&h, &p), 
+                &transpose(&h)
+            ), 
+            &r
+        );
+    let k = dot_to_2x2(&k_num, &inv_2x2(&k_den));
+
+    let x = add_2x1(
+        &x, 
+        &dot_to_2x1(
+            &k, 
+            &sub_2x1(
+                &y, 
+                &dot_to_2x1(&h, &x)
+            )
+        )
+    );
+
+    let p = sub_2x2(
+        &p,
+        &dot_to_2x2(
+            &k,
+            &dot_to_2x2(&h, &p)
+        )
+    );
+
+    (x, p)
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::{predict, dot_to_2x1, dot_to_2x2, transpose, add_2x2, sub_2x1, add_2x1, sub_2x2, inv_2x2};
+
+    #[test]
+    fn test_transpose() {
+        let a = (
+            (1.0, 2.0),
+            (3.0, 4.0)
+        );
+        let res = (
+            (1.0, 3.0),
+            (2.0, 4.0)
+        );
+        assert_eq!(transpose(&a), res);
+    }
+
+    #[test]
+    fn test_dot_to_2x1() {
+        let a = (
+            (1.0, 2.0),
+            (3.0, 4.0)
+        );
+        let b = (
+            (5.0,),
+            (6.0,)
+        );
+        let res = (
+            (17.0,),
+            (39.0,)
+        );
+        assert_eq!(dot_to_2x1(&a, &b), res);
+    }
+
+    #[test]
+    fn test_dot_to_2x2() {
+        let a = (
+            (1.0, 2.0),
+            (3.0, 4.0)
+        );
+        let b = (
+            (5.0, 6.0),
+            (7.0, 8.0)
+        );
+        let res = (
+            (19.0, 22.0),
+            (43.0, 50.0)
+        );
+        assert_eq!(dot_to_2x2(&a, &b), res);
+    }
+
+    #[test]
+    fn test_add_2x2() {
+        let a = (
+            (1.0, 2.0),
+            (3.0, 4.0)
+        );
+        let b = (
+            (5.0, 6.0),
+            (7.0, 8.0)
+        );
+        let res = (
+            (6.0, 8.0),
+            (10.0, 12.0)
+        );
+        assert_eq!(add_2x2(&a, &b), res);
+    }
+    
+    #[test]
+    fn test_sub_2x1() {
+        let a = (
+            (1.0,),
+            (2.0,)
+        );
+        let b = (
+            (3.0,),
+            (4.0,)
+        );
+        let res = (
+            (-2.0,),
+            (-2.0,)
+        );
+        assert_eq!(sub_2x1(&a, &b), res);
+    }
+
+    #[test]
+    fn test_add_2x1() {
+        let a = (
+            (1.0,),
+            (2.0,)
+        );
+        let b = (
+            (3.0,),
+            (4.0,)
+        );
+        let res = (
+            (4.0,),
+            (6.0,)
+        );
+        assert_eq!(add_2x1(&a, &b), res);
+    }
+    #[test]
+    fn test_sub_2x2() {
+        let a = (
+            (1.0, 2.0),
+            (3.0, 4.0)
+        );
+        let b = (
+            (5.0, 6.0),
+            (7.0, 8.0)
+        );
+        let res = (
+            (-4.0, -4.0),
+            (-4.0, -4.0)
+        );
+        assert_eq!(sub_2x2(&a, &b), res);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_inv_2x2_should_panic_if_determinant_is_0() {
+        let a = (
+            (1.0, 1.0),
+            (2.0, 2.0)
+        );        
+        inv_2x2(&a);
+    }
+    
+
+    #[test]
+    fn test_predict() {
+
+        let x = (
+            (1.0,),
+            (2.0,)
+        );
+
+        let p = (
+            (3.0, 0.0),
+            (0.0, 4.0)
+        );
+
+        let a = (
+            (2.0, 2.0),
+            (0.0, 2.0)
+        );
+        let res = (
+            ((6.0,), (4.0,)),
+            ((28.0, 0.0), (0.0, 16.0)),
+        );
+
+        assert_eq!(predict(&x, &p, &a), res);
+    }
+}
+    
+
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,131 @@
-use ndarray::prelude::*;
+use rand::prelude::*;
+use image::{ImageBuffer, Rgb};
+
+
+type M2x1 = (
+    (f32,),
+    (f32,)
+);
+type M2x2 = (
+    (f32, f32),
+    (f32, f32)
+);
+
+mod kalman;
+
 
 fn main() {
-    println!("Hello, world!");
+
+    //tableau blanc spour l'image
+    //un pixel noir est détecté sur une ligne à la colonne 0
+    //->détecter des aberrations sur une ligne pas complètement droite à partir de colonne 1
+    //prédiction => ligne attendue
+    //générateur de point de mesure ligne/colonne +/- rand (très léger) + aberrations pendant n pixels à partir d'un seuil
+    //Vk is the innovation or the measurement residual on time step k au carré comparé à une valeur prédéfinie = 10 fois la variance => aberration
+    //mise à jour si ok
+    //enregistrement d'un fichier image ligne prédite est décalée vers le bas de n pixels
+
+    let mut img = ImageBuffer::from_fn(512, 512, |x, y| {
+        image::Rgb([255, 255, 255])
+    });
+    
+    match simulator() {
+        Err(e) => println!("{:?}", e),
+        Ok(v) => {
+            for p in v {
+                img.put_pixel(p.0, p.1, Rgb([255, 0, 0])); 
+                img.put_pixel(p.0, p.2, Rgb([0, 0, 255]));                
+            }           
+        }
+    }
+
+    img.save("rustscanscore.png");
+
 }
 
-fn predict(x: &Array2<u64>, y: &Array2<u64>) -> Array2<u64> {  
-    x.dot(y)
+fn simulator() -> Result<Vec<(u32, u32, u32)>, &'static str> {
+
+    let measurements = sample_line_gen();
+    let mut measurement_iter = measurements.iter();
+
+    let mut last_measure= *measurement_iter.next().ok_or("Cannot init simulator with an empty vector")?; 
+
+    let mut dt = 1.0;
+
+    let mut x = (
+        (last_measure,),
+        (0.0,)
+    );
+
+        
+    let mut p = (
+        (1.0, 0.0),
+        (0.0, 1.0)
+    );
+
+    let h = (
+        (1.0, 0.0),
+        (0.0, 1.0)
+    );
+    let r = (
+        (1.0, 0.0),
+        (0.0, 1.0)
+    );
+
+    let mut vec = Vec::<(u32, u32, u32)>::new();
+    vec.push((dt as u32, last_measure as u32, last_measure as u32));
+
+
+    for measure in measurement_iter {
+
+        let a = (
+            (1.0, 1.0),
+            (0.0, 1.0)
+        );
+
+        let (t_x, t_p) = kalman::predict(&x, &p, &a);
+        x = t_x;
+        p = t_p;
+
+        let y = ( 
+            (*measure, ),
+            (*measure - last_measure, )
+        );
+
+        vec.push(
+            (dt as u32, *measure as u32, x.0.0 as u32)
+        );
+
+        let (t_x, t_p) = kalman::update(&x, &p, &y, &h, &r);    
+        x = t_x;
+        p = t_p;
+
+        last_measure = *measure;      
+        dt += 1.0;  
+        
+        
+    }
+    
+    Ok(vec)
 }
 
-#[test]
-fn test_predict() {
-    let x = array![
-        [1, 2],
-        [3, 4]
-    ];
-    let y = array![
-        [1, 2],
-        [3, 4]
-    ];
-    let x_multiply_y = array![
-        [7, 10],
-        [15, 22]
-    ];
+fn sample_line_gen() -> Vec<f32> {
+    let inc = 1.0;
+    let step = 10;
+    let mut x = 250.0;
+    let mut line = Vec::new();
 
-    assert_eq!(predict(&x,&y), x_multiply_y);
+    for y in 1..501 {
+        let variability = match y > 100 && y < 120 {
+            true => rand::random::<i16>()/5000,
+            false => rand::random::<i16>()/10000
+        };
+                
+        line.push(
+           x + variability as f32
+        );
+        if y%step == 0 {x = x + inc;}
+    }
+
+    line
 }
