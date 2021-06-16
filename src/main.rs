3f0941405e41f74ee58f76ec01a43e49202eed9b
Basile du Plessis <basile.duplessis@gmail.com>
2021-06-16 15:20:19 +0200

    Prepare line detection for tests with sample images

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,12 @@
-use std::ops::IndexMut;
 use std::usize;
-use std::cmp::Ordering;
 
+mod staves;
 mod kalman;
-mod simulator;
+
 
 
 fn main() {
-
-    let img = image::open("score_sample/score_sample2.png").unwrap();
-    let img_gray = img.into_luma8();
-
-    let width = img_gray.width() as usize;
-    let height = img_gray.height() as usize;
-    let buffer_size = width * height;
-
-
-    let mut buffer_vertical = vec![255; buffer_size];
-
-  
-    for id_horizontal in 0..(buffer_size-1) {
-        let id_vertical = buffer_id_swap(id_horizontal, width, height);
-        buffer_vertical[id_vertical] = *img_gray.get(id_horizontal).unwrap_or(&255);
-    }     
-
-    let mut staves = Vec::<Staff>::new();
-
-    for (y, buff) in buffer_vertical.chunks(height).enumerate() {
-
-        let pixel_positions = buff
-            .iter()
-            .enumerate()
-            .filter(|(_, v)| **v == 0)
-            .map(|(x, _)| x + 1)
-            .collect::<Vec<usize>>();
-
-        if pixel_positions.len() == 0 {continue;}
-
-        let y = y + 1;
-        
-        let staff_predictions = staves
-            .iter()
-            .map(|staff| staff.get_prediction(y))
-            .collect::<Vec<(f32, f32)>>();
-
-        let matches = pixel_positions
-            .iter()
-            .map(
-                |p|
-                match_position(&staff_predictions, &p)
-            )
-            .collect::<Vec<Option<usize>>>();
-
-        let matched_pixels = matches.iter()
-            .enumerate()
-            .filter(|(_, opt)| opt.is_some())
-            .map(|(i, opt)| (pixel_positions[i], opt.unwrap()))
-            .collect::<Vec<(usize, usize)>>();
-
-        for (s, xs) in group_by_equal_value(matched_pixels) {
-            staves[s].push_pixels(xs, y);
-        }
-        
-        let unmatched_pixels = matches
-            .iter()
-            .enumerate()    
-            .filter(|(_, opt)| opt.is_none())
-            .map(|(i, _)| pixel_positions[i])
-            .collect::<Vec<usize>>();
-        
-        for xs in group_by_incremental_values(unmatched_pixels) {
-            staves.push(Staff::new(xs, y))
-        }
-
-    }
-
-
-    println!("{:?}", staves);
+ 
 
     /*
     for (id, y) in buffer_x.iter().enumerate() {
@@ -99,191 +29,30 @@ fn buffer_id_swap(id: usize, a: usize, b: usize) -> usize {
     dim1 * b + dim2
 }
 
-fn match_position(predictions: &Vec<(f32, f32)>, position: &usize) -> Option<usize> {
-    let mut result = predictions
-        .iter()
-        .enumerate()
-        .map(|(id, t)| (id, (*position as f32 - t.0.round()).abs(), t.1))
-        .filter(|(_, diff, _)| *diff <= 1.0)
-        .collect::<Vec<(usize, f32, f32)>>();
-   
-    result.sort_by(
-        |a,b| 
-        a.2
-            .partial_cmp(&b.2)
-            .unwrap_or(std::cmp::Ordering::Equal)
-    );
-
-    match result.first() {
-        None => None,
-        Some(prediction) => Some(prediction.0)
-    }
-}
-
-fn group_by_incremental_values(vec: Vec<usize>) -> Vec<Vec<usize>> {
-    let mut res:Vec<Vec<usize>> = Vec::new();
-
-    if vec.len() > 0 {
-        res.push(vec![vec[0]]);
-    }    
-
-    for s in vec.windows(2) {         
-        match s {
-            [current, next] if next-current==1 => 
-                match res.last_mut() {
-                    Some(last) => last.push(*next),
-                    _ => ()
-                },
-            [_, next] => res.push(vec![*next]),
-            _ => ()
-        }
-    }
-    res
-}
-
-fn group_by_equal_value(vec: Vec<(usize, usize)>) -> Vec<(usize, Vec<usize>)> {
-    let mut res:Vec<(usize, Vec<usize>)> = Vec::new();
-       
-    let mut iter = vec;
-    
-    iter.sort_by(
-        |a, b| 
-        a.1.partial_cmp(&b.1)
-        .unwrap_or(std::cmp::Ordering::Equal)
-    );
-
-    if iter.len() > 0 {
-        res.push((iter[0].1, vec![iter[0].0]));
-    }
-
-    for s in iter.windows(2) {
-        match s {
-            [current, next] if current.1==next.1 =>
-                match res.last_mut() {
-                    Some(last) => last.1.push(next.0),
-                    _ => ()
-                },
-            [_, next] => res.push((next.1, vec![next.0])),
-            _ => ()
-         }
-    }
-
-    res
-}
-
 #[cfg(test)]
 mod tests {
+
     use super::*;
 
-    #[test]
-    fn test_group_by_equal_value_sort_values() {
-        let vec = vec![
-            (4, 3),
-            (3, 4),
-            (2, 3),
-            (1, 4)
-        ];
-        let res= vec![
-            (3, vec![4,2]),
-            (4, vec![3,1])         
-        ];
+    fn prepare_img(img: &str) -> (Vec<u8>, usize, usize) {
+        let img = image::open(img).unwrap();
+        let img_gray = img.into_luma8();
+    
+        let width = img_gray.width() as usize;
+        let height = img_gray.height() as usize;
+        let buffer_size = width * height;
+    
+    
+        let mut buffer_vertical = vec![255; buffer_size];
+    
+      
+        for id_horizontal in 0..(buffer_size-1) {
+            let id_vertical = buffer_id_swap(id_horizontal, width, height);
+            buffer_vertical[id_vertical] = *img_gray.get(id_horizontal).unwrap_or(&255);
+        }
 
-        assert_eq!(group_by_equal_value(vec), res);
+        (buffer_vertical, width, height)
     }
-
-    #[test]
-    fn test_group_by_equal_value_sort_values_2() {
-        let vec = vec![
-            (0, 4),
-            (3, 4),
-            (2, 4),
-            (1, 3),
-            (8, 4),
-        ];
-        let res= vec![
-            (3, vec![1]),
-            (4, vec![0,3,2,8])         
-        ];
-
-        assert_eq!(group_by_equal_value(vec), res);
-    }
-
-    #[test]
-    fn test_group_by_equal_value_return_empty_if_empty() {
-        let vec = Vec::new();
-        let res= Vec::new();
-
-        assert_eq!(group_by_equal_value(vec), res);
-    }
-
-
-
-    #[test]
-    fn test_group_by_consecutive_value() {
-        let vec = vec![0,2,3,4,6,7];
-        let res= vec![
-            vec![0],
-            vec![2, 3, 4],
-            vec![6, 7]
-        ];
-
-        assert_eq!(group_by_incremental_values(vec), res);
-    }
-
-    #[test]
-    fn test_group_by_consecutive_value_handle_one_value() {
-        let vec = vec![0];
-        let res= vec![
-            vec![0]
-        ];
-
-        assert_eq!(group_by_incremental_values(vec), res);
-    }
-
-    #[test]
-    fn test_group_by_consecutive_value_return_empty_for_empty() {
-        let vec = vec![];
-        let res: Vec<Vec<usize>> = Vec::new();
-
-        assert_eq!(group_by_incremental_values(vec), res);
-    }
-
-    #[test]
-    fn test_match_position_foster_horizontality() {
-        let predictions = vec![
-            (1.0, 1.0),
-            (0.0, 0.0)        
-        ];
-        let position = 1;
-        assert_eq!(match_position(&predictions, &position), Some(1));
-    }
-    #[test]
-    fn test_match_position_round_predicition() {
-        let predictions = vec![
-            (0.49, 1.0)
-        ];
-        let position = 2;
-        assert_eq!(match_position(&predictions, &position), None);
-        let predictions = vec![
-            (0.50, 1.0)
-        ];
-        let position = 2;
-        assert_eq!(match_position(&predictions, &position), Some(0));
-    }
-    #[test]
-    fn test_match_position_follow_sorting_in_eq_condition() {
-        let pred1 = vec![        
-            (2.0, 0.0),
-            (0.0, 0.0)
-        ];
-        let pred2 = vec![        
-            (0.0, 0.0),
-            (2.0, 0.0)
-        ];
-        let position = 1;
-        assert_eq!(match_position(&pred1, &position), match_position(&pred2, &position));
-    }
-
 
     #[test]
     fn test_buffer_idx_swap_first_one_last_one_keep_same() {
@@ -303,87 +72,24 @@ mod tests {
         assert_eq!(buffer_id_swap(idx_height, width, height), idx_width);
         assert_eq!(buffer_id_swap(idx_width, height, width), idx_height);
     }
-}
 
+    #[test]
+    fn test_one_full_line_get_one_staff_with_10_items() {
+        let (buffer, _, height) = prepare_img("score_sample/single_line_top.png");
 
-#[derive(Debug)]
-struct Staff {
-    x: kalman::M2x1,
-    p: kalman::M2x2,
-    buffer: Vec<(Vec<usize>, usize)>
-}
+        let staves = staves::detect_staves(buffer, height);
 
-impl Staff {
-    fn new(xs: Vec<usize>, y: usize) -> Staff {   
-        let mean: f32 = xs.iter().sum::<usize>() as f32 / xs.len() as f32;
-        Staff {
-            x: (
-                (mean,),
-                (0.0,)
-            ),
-            p: (
-                (1.0, 0.0),
-                (0.0, 1.0)
-            ),
-            buffer: vec![(xs, y)]
-        }
-    }
-    
-    const H:kalman::M2x2 = (
-        (1.0, 0.0),
-        (0.0, 1.0)
-    );
-    const R:kalman::M2x2 = (
-        (1.0, 0.0),
-        (0.0, 1.0)
-    );
-
-
-    fn get_prediction(&self, y: usize) -> (f32, f32) {
-        let last_y = self.buffer.last().unwrap().1;
-        let a = (
-            (1.0, (y - last_y) as f32),
-            (0.0, 1.0)
-        );        
-        let (t_x, _) = kalman::predict(&self.x, &self.p, &a);
-        (t_x.0.0, t_x.1.0)
+        assert_eq!(staves.len(), 1);
+        assert_eq!(staves[0].buffer.len(), 10);
     }
 
-    fn push_pixels(&mut self, xs: Vec<usize>, y: usize) {
-        
-        let default = (xs.clone(), y.clone());
+    #[test]
+    fn test_full_black() {
+        let (buffer, _, height) = prepare_img("score_sample/full_black.png");
 
-        let last_pixels = self.buffer.last().unwrap_or(&default);
+        let staves = staves::detect_staves(buffer, height);
 
-        let x_mean = xs.iter().sum::<usize>() as f32 / xs.len() as f32;
-        let last_x_mean = last_pixels.0.iter().sum::<usize>() as f32 / last_pixels.0.len() as f32;
-
-        let a = (
-            (1.0,  y as f32 - last_pixels.1 as f32),
-            (0.0, 1.0)
-        );
-
-        let (t_x, t_p) =
-            kalman::predict(&self.x, &self.p, &a);
-        
-        let speed = 
-            x_mean - last_x_mean
-            /
-            (y as f32 - last_pixels.1 as f32);
-
-        let measure = ( 
-            (x_mean as f32, ),
-            (speed, )
-        );
-
-        let (t_x, t_p) =
-            kalman::update(&t_x, &t_p, &measure, &Staff::H, &Staff::R);   
-        
-        self.x = t_x;
-        self.p = t_p;
-
-        self.buffer.push((xs, y));
-
+        println!("{:?}", staves);
     }
 
 }
---
+++ src/staves.rs
@@ -0,0 +1,366 @@
+struct Prediction {    
+    from_y: f32,
+    x: f32,
+    bias: f32
+}
+
+#[derive(Debug)]
+pub struct Staff {
+    x: crate::kalman::M2x1,
+    p: crate::kalman::M2x2,
+    pub buffer: Vec<(Vec<usize>, usize)>
+}
+
+impl Staff {
+
+    fn new(xs: Vec<usize>, y: usize) -> Staff {   
+        let mean: f32 = xs.iter().sum::<usize>() as f32 / xs.len() as f32;
+        Staff {
+            x: (
+                (mean,),
+                (0.0,)
+            ),
+            p: (
+                (1.0, 0.0),
+                (0.0, 1.0)
+            ),
+            buffer: vec![(xs, y)]
+        }
+    }
+    
+    const H:crate::kalman::M2x2 = (
+        (1.0, 0.0),
+        (0.0, 1.0)
+    );
+    const R:crate::kalman::M2x2 = (
+        (1.0, 0.0),
+        (0.0, 1.0)
+    );
+
+
+    fn get_prediction(&self, y: usize) -> Prediction {
+        let last_y = self.buffer.last().unwrap().1 as f32;
+        
+        let a = (
+            (1.0, y as f32 - last_y),
+            (0.0, 1.0)
+        );
+
+        let (t_x, _) = crate::kalman::predict(&self.x, &self.p, &a);
+
+        Prediction {
+            from_y: last_y,
+            x: t_x.0.0,
+            bias: t_x.1.0
+        }
+                
+    }
+
+    fn push_pixels(&mut self, xs: Vec<usize>, y: usize) {
+        
+        let default = (xs.clone(), y.clone());
+
+        let last_pixels = self.buffer.last().unwrap_or(&default);
+
+        let x_mean = xs.iter().sum::<usize>() as f32 / xs.len() as f32;
+        let last_x_mean = last_pixels.0.iter().sum::<usize>() as f32 / last_pixels.0.len() as f32;
+
+        let a = (
+            (1.0,  y as f32 - last_pixels.1 as f32),
+            (0.0, 1.0)
+        );
+
+        let (t_x, t_p) =
+        crate::kalman::predict(&self.x, &self.p, &a);
+        
+        let speed = 
+            x_mean - last_x_mean
+            /
+            (y as f32 - last_pixels.1 as f32);
+
+        let measure = ( 
+            (x_mean as f32, ),
+            (speed, )
+        );
+
+        let (t_x, t_p) =
+        crate::kalman::update(&t_x, &t_p, &measure, &Staff::H, &Staff::R);   
+        
+        self.x = t_x;
+        self.p = t_p;
+
+        self.buffer.push((xs, y));
+
+    }
+
+}
+
+pub fn detect_staves(buffer_vertical:Vec<u8>, height: usize) -> Vec<Staff> {
+    let mut staves = Vec::<Staff>::new();
+
+    for (y, buff) in buffer_vertical.chunks(height).enumerate() {
+
+        let pixel_positions = buff
+            .iter()
+            .enumerate()
+            .filter(|(_, v)| **v == 0)
+            .map(|(x, _)| x + 1)
+            .collect::<Vec<usize>>();
+
+        if pixel_positions.len() == 0 {continue;}
+
+        let y = y + 1;
+        
+        let staff_predictions = staves
+            .iter()
+            .map(|staff| staff.get_prediction(y))
+            .collect::<Vec<Prediction>>();
+
+        let matches = pixel_positions
+            .iter()
+            .map(
+                |x|
+                match_position(&staff_predictions, x, &y)
+            )
+            .collect::<Vec<Option<usize>>>();
+
+        let matched_pixels = matches.iter()
+            .enumerate()
+            .filter(|(_, opt)| opt.is_some())
+            .map(|(i, opt)| (pixel_positions[i], opt.unwrap()))
+            .collect::<Vec<(usize, usize)>>();
+
+        for (s, xs) in group_by_equal_value(matched_pixels) {
+            staves[s].push_pixels(xs, y);
+        }
+        
+        let unmatched_pixels = matches
+            .iter()
+            .enumerate()    
+            .filter(|(_, opt)| opt.is_none())
+            .map(|(i, _)| pixel_positions[i])
+            .collect::<Vec<usize>>();
+        
+        for xs in group_by_incremental_values(unmatched_pixels) {
+            staves.push(Staff::new(xs, y))
+        }
+
+    }
+
+    staves
+    
+}
+
+
+
+fn match_position(predictions: &Vec<Prediction>, x: &usize, y: &usize) -> Option<usize> {
+    let mut result = predictions
+        .iter()
+        .enumerate()
+        .filter(|(_, pred)| (*x as f32 - pred.x.round()).abs() <= 1.0)
+        .map(
+            |(id, pred)|
+            (id, *y as f32 - pred.from_y, pred.bias))
+        .collect::<Vec<(usize, f32, f32)>>();
+   
+    result.sort_by(
+        |a,b| 
+        a.1
+            .partial_cmp(&b.1)
+            .unwrap_or(std::cmp::Ordering::Equal)
+            .then(
+                a.2.partial_cmp(&b.2).unwrap_or(std::cmp::Ordering::Equal)
+            )
+    );
+
+    match result.first() {
+        None => None,
+        Some(r) => Some(r.0)
+    }
+}
+
+fn group_by_incremental_values(vec: Vec<usize>) -> Vec<Vec<usize>> {
+    let mut res:Vec<Vec<usize>> = Vec::new();
+
+    if vec.len() > 0 {
+        res.push(vec![vec[0]]);
+    }    
+
+    for s in vec.windows(2) {         
+        match s {
+            [current, next] if next-current==1 => 
+                match res.last_mut() {
+                    Some(last) => last.push(*next),
+                    _ => ()
+                },
+            [_, next] => res.push(vec![*next]),
+            _ => ()
+        }
+    }
+    res
+}
+
+fn group_by_equal_value(vec: Vec<(usize, usize)>) -> Vec<(usize, Vec<usize>)> {
+    let mut res:Vec<(usize, Vec<usize>)> = Vec::new();
+       
+    let mut iter = vec;
+    
+    iter.sort_by(
+        |a, b| 
+        a.1.partial_cmp(&b.1)
+        .unwrap_or(std::cmp::Ordering::Equal)
+    );
+
+    if iter.len() > 0 {
+        res.push((iter[0].1, vec![iter[0].0]));
+    }
+
+    for s in iter.windows(2) {
+        match s {
+            [current, next] if current.1==next.1 =>
+                match res.last_mut() {
+                    Some(last) => last.1.push(next.0),
+                    _ => ()
+                },
+            [_, next] => res.push((next.1, vec![next.0])),
+            _ => ()
+         }
+    }
+
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_group_by_equal_value_sort_values() {
+        let vec = vec![
+            (4, 3),
+            (3, 4),
+            (2, 3),
+            (1, 4)
+        ];
+        let res= vec![
+            (3, vec![4,2]),
+            (4, vec![3,1])         
+        ];
+
+        assert_eq!(group_by_equal_value(vec), res);
+    }
+
+    #[test]
+    fn test_group_by_equal_value_sort_values_2() {
+        let vec = vec![
+            (0, 4),
+            (3, 4),
+            (2, 4),
+            (1, 3),
+            (8, 4),
+        ];
+        let res= vec![
+            (3, vec![1]),
+            (4, vec![0,3,2,8])         
+        ];
+
+        assert_eq!(group_by_equal_value(vec), res);
+    }
+
+    #[test]
+    fn test_group_by_equal_value_return_empty_if_empty() {
+        let vec = Vec::new();
+        let res= Vec::new();
+
+        assert_eq!(group_by_equal_value(vec), res);
+    }
+
+
+
+    #[test]
+    fn test_group_by_consecutive_value() {
+        let vec = vec![0,2,3,4,6,7];
+        let res= vec![
+            vec![0],
+            vec![2, 3, 4],
+            vec![6, 7]
+        ];
+
+        assert_eq!(group_by_incremental_values(vec), res);
+    }
+
+    #[test]
+    fn test_group_by_consecutive_value_handle_one_value() {
+        let vec = vec![0];
+        let res= vec![
+            vec![0]
+        ];
+
+        assert_eq!(group_by_incremental_values(vec), res);
+    }
+
+    #[test]
+    fn test_group_by_consecutive_value_return_empty_for_empty() {
+        let vec = vec![];
+        let res: Vec<Vec<usize>> = Vec::new();
+
+        assert_eq!(group_by_incremental_values(vec), res);
+    }
+
+    #[test]
+    fn test_match_position_foster_continuity_and_horizontality() {
+        let predictions = vec![
+            Prediction {x: 1.0, from_y: 1.0, bias: 1.0},
+            Prediction {x: 2.0, from_y: 3.0, bias: 0.0},        
+        ];
+        let x = 1;
+        let y = 4;
+        assert_eq!(match_position(&predictions, &x, &y), Some(1));
+    }
+     
+    #[test]
+    fn test_match_position_round_predicition() {
+        let predictions = vec![
+            Prediction {x: 0.49, from_y: 1.0, bias: 0.0},
+        ];
+        let x = 2;
+        let y = 2;
+        assert_eq!(match_position(&predictions, &x, &y), None);
+
+        let predictions = vec![
+            Prediction {x: 0.50, from_y: 1.0, bias: 0.0},
+        ];
+        let x = 2;
+        let y = 2;
+        assert_eq!(match_position(&predictions, &x, &y), Some(0));
+    }
+    #[test]
+    fn test_match_position_disadvantage_distant_staff() {
+        let predictions = vec![
+            Prediction {x: 1., from_y: 1.0, bias: 0.0},    
+            Prediction {x: 2., from_y: 2.0, bias: 0.5}
+        ];
+        let x = 1;
+        let y = 3;
+        assert_eq!(match_position(&predictions, &x, &y), Some(1));
+    }
+    #[test]
+    fn test_match_position_follow_sorting_in_eq_condition() {
+        let pred1 = vec![      
+            Prediction {x: 3., from_y: 1.0, bias: 0.0},    
+            Prediction {x: 1., from_y: 1.0, bias: 0.0}  
+        ];
+        let pred2 = vec![        
+            Prediction {x: 1., from_y: 1.0, bias: 0.0},
+            Prediction {x: 3., from_y: 1.0, bias: 0.0}
+        ];
+        let x = 2;
+        let y = 2;
+        assert_eq!(match_position(&pred1, &x, &y), Some(0));
+        assert_eq!(match_position(&pred2, &x, &y), Some(0));
+    }
+
+
+   
+}
+
